bf8dc1b45315f6571d3dc72993f47ec819768622
WodaMineralna <raknieborak65@gmail.com>
2025-11-03 22:56:35 +0100

    chore(locals): avoid setting `res.locals.csrfToken` for `/api` requests
    - Update `attachLocals` to not set `csrfToken` when `req.originalUrl` starts with `/api`
    - Prevents undefined token rendering in views and clarifies that API responses do not use CSRF

--- middleware/attachLocals.js
+++ middleware/attachLocals.js
@@ -1,6 +1,7 @@
 export default (req, res, next) => {
   res.locals.loggedIn = req.session.loggedIn;
-  res.locals.csrfToken = req.csrfToken();
+  if (!req.originalUrl.startsWith("/api"))
+    res.locals.csrfToken = req.csrfToken();
   res.locals.oldInput = req.body.oldInput ?? {};
   res.locals.errorMessage = req.flash("error");
   res.locals.infoMessage = req.flash("info");
