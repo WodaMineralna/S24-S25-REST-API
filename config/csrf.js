d0d623834f53d665a5f0ee250387c459cabf73a7
WodaMineralna <raknieborak65@gmail.com>
2025-11-03 22:55:37 +0100

    chore(csrf): bypass CSRF for `/api` routes while adding REST API support
    - Skip CSRF middleware for endpoints under /api to allow REST testing with JSON clients
    - Keep CSRF enabled for server-rendered MVC routes
    - This is intentional for the REST migration and will be revisited when switching to JWT auth

--- config/csrf.js
+++ config/csrf.js
@@ -1,4 +1,7 @@
 import csrf from "csurf";
 const csrfProtection = csrf();
 
-export default csrfProtection;
+export default function (req, res, next) {
+  if (req.originalUrl.startsWith("/api")) return next();
+  return csrfProtection(req, res, next);
+}
