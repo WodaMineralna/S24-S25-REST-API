8ede423d4eb35802562503c44c67288f0bd6812b
WodaMineralna <raknieborak65@gmail.com>
2025-11-08 22:55:57 +0100

    chore(api): register api-shop routes in `api-index.js`
    - Imported and mounted `api-shop.js` under `/shop` prefix - MVC didn't use prefix for non admin-related routes
    - Keeps structure consistent with `/api/admin` routes

--- routes/api-index.js
+++ routes/api-index.js
@@ -2,7 +2,9 @@ import express from "express";
 const router = express.Router();
 
 import apiAdminRoutes from "./api-admin.js";
+import apiShopRoutes from "./api-shop.js";
 
 router.use("/admin", apiAdminRoutes);
+router.use("/shop", apiShopRoutes);
 
 export default router;
